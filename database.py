c8f0298143c24d0036f69a740cf1644263f4b562
JoaoVitorDuarte28 <121297490+JoaoVitorDuarte28@users.noreply.github.com>
2022-12-23 10:37:11 -0300

    ALTERADO DADOS
    TESTE

--- database.py
+++ database.py
@@ -2,10 +2,9 @@ import pyodbc
 # Some other example server values are
 # server = 'localhost\sqlexpress' # for a named instance
 # server = 'myserver,port' # to specify an alternate port
-server = '10.100.100.5' 
-database = 'dbSicred_Proj' 
-username = 'usr_sicred' 
-password = '#Homologsicr3d#' 
+server =  
+database = 
+usernam
 # ENCRYPT defaults to yes starting in ODBC Driver 18. It's good to always specify ENCRYPT=yes on the client side to avoid MITM attacks.
 cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';ENCRYPT=yes;UID='+username+';PWD='+ password)
 conn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD=password')
